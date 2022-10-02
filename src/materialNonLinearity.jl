77c2c19604a8b13e368f02fbb5bd3869642d3da2
joaquinv <joaquinv@fing.edu.uy>
2022-10-02 12:49:54 -0300

    agrego test

--- src/materialNonLinearity.jl
+++ src/materialNonLinearity.jl
@@ -1,5 +1,18 @@
 module materialNonLinearity
 
-greet() = print("Hello World!")
+export hello, domath
+
+"""
+    hello(who::String)
+Return "Hello, `who`".
+"""
+hello(who::String) = "Hello, $who"
+
+"""
+    domath(x::Number)
+Return `x + 5`.
+"""
+domath(x::Number) = x + 5
+
 
 end # module
