596b5b3ab3037fef40d8531e084c1fae61c1948e
Documenter.jl <documenter@juliadocs.github.io>
2022-10-02 15:51:09 +0000

    build based on 12f5ab4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#materialNonLinearity","page":"materialNonLinearity","title":"materialNonLinearity","text":"","category":"section"},{"location":"","page":"materialNonLinearity","title":"materialNonLinearity","text":"Example Julia package repo.","category":"page"},{"location":"","page":"materialNonLinearity","title":"materialNonLinearity","text":"Modules = [materialNonLinearity]","category":"page"}]
+}
