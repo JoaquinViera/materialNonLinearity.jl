20b328ed8f0a54d155e9039a2318537baecb2fa4
JoaquinViera <45524519+JoaquinViera@users.noreply.github.com>
2022-10-11 18:40:41 -0300

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,12 @@
 using Documenter, materialNonLinearity
 
-makedocs(modules=[materialNonLinearity], sitename="materialNonLinearity.jl")
+makedocs(
+  modules=[materialNonLinearity], 
+  sitename="materialNonLinearity.jl",
+  pages=[
+        "Home" => "index.md"
+        ]
+)
 
 deploydocs(
   repo="github.com/JoaquinViera/materialNonLinearity.jl.git",
