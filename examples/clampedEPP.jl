bcbf0d8448fd71cd7c0b1ad0423c367afcd819b4
joaquinv <joaquinv@fing.edu.uy>
2023-01-07 19:38:33 -0300

    clamped beam example added

--- examples/clampedEPP.jl
+++ examples/clampedEPP.jl
@@ -3,7 +3,7 @@
 # ===============================================
 
 # Load solver module
-using materialNonLinearity, Plots, LinearAlgebra, Polynomials
+using materialNonLinearity, Plots, LinearAlgebra, FastGaussQuadrature, Printf
 
 # example name
 problemName = "ClampedEPP"
@@ -16,11 +16,10 @@ K = 0
 
 # Gauss points
 ne = 16 # element
-ns = 16 # section
+ns = 20 # section
 
 # Materials struct
 StrMaterialModels = IsotropicBiLinear(E, σY, K, ne, ns)
-StrMaterialModels = LinearElastic(E, ne, ns)
 
 # Define section
 # =======================================
@@ -35,7 +34,7 @@ StrSections = Rectangle(; b, h)
 
 # Nodes
 L = 1.5
-nnodes = 3 * 4 + 1
+nnodes = 3 * 9 + 1
 xcoords = collect(LinRange(0, L, nnodes))
 ycoords = zeros(length(xcoords))
 Nodes = hcat(xcoords, ycoords)
@@ -83,7 +82,7 @@ tolf = 1e-6 # Tolerance of internal forces
 initialDeltaLambda = 1e-5 #
 
 # arcLengthIncrem = vcat(ones(18) * 1e-3, ones(80) * 1e-4) # 21 node
-arcLengthIncrem = vcat(ones(2) * 1e-3) # 21 node
+arcLengthIncrem = vcat(ones(18) * 3e-4, ones(60) * 1e-4) # 21 node
 nLoadSteps = length(arcLengthIncrem) # Number of load increments
 dof1 = nod1 * 3 - 1
 dof2 = nod2 * 3 - 1
@@ -93,17 +92,17 @@ scalingProjection = 1 #
 # Numerical method settings struct
 # StrAnalysisSettings = ArcLength(tolk, tolu, tolf, nLoadSteps, initialDeltaLambda, arcLengthIncrem, controlDofs, scalingProjection)
 
-#
-nLoadSteps = 10 # Number of load increments
+# #
+nLoadSteps = 260 # Number of load increments
 loadFactorsVec = collect(1:nLoadSteps) # Load scaling factors
 
-# Numerical method settings struct
+# # Numerical method settings struct
 StrAnalysisSettings = NewtonRaphson(tolk, tolu, tolf, loadFactorsVec)
 #
 
 # Stress Array
 # =======================================
-elems = []
+elems = [1, nod1]
 xG_Rel_Ind = 0
 
 StrStressArray = StressArraySets(elems, xG_Rel_Ind)
@@ -112,7 +111,7 @@ StrStressArray = StressArraySets(elems, xG_Rel_Ind)
 # Process model parameters
 # ===============================================
 
-sol, time, IterData = solver(StrSections, StrMaterialModels, StrMesh, StrBoundaryConds, StrAnalysisSettings, problemName, StrStressArray)
+sol, time, IterData, σArr = solver(StrSections, StrMaterialModels, StrMesh, StrBoundaryConds, StrAnalysisSettings, problemName, StrStressArray)
 
 println(IterData.stopCrit)
 
@@ -163,12 +162,13 @@ Msupp = 2 * pVec * L / 9
 
 # Analytical solution M-κ
 # --------------------------------
-Mana = zeros(nLoadSteps)
+
 κₑ = 2 * σY / (E * h)
 C = E * K / (E + K)
 epsY = σY / E
 eps_ast = epsY - σY / C
 kappa_ast = 2 * eps_ast / h
+Mana = zeros(nLoadSteps)
 elem = convert(Integer, nod1)
 for i in 1:nLoadSteps
     κₖ = abs(kappaHistElem[elem, i])
@@ -181,6 +181,22 @@ for i in 1:nLoadSteps
         z0 = eps_ast / κₖ
         zs = z0 - zy
         Mana[i] = 2 * zy^2 * σY * b / 3 + zs * σY * b * (zy + (z0 - zy) / 3)
+    end
+end
+
+ManaR = zeros(nLoadSteps)
+elem = 1
+for i in 1:nLoadSteps
+    κₖ = abs(kappaHistElem[elem, i])
+    if κₖ <= κₑ
+        ManaR[i] = E * StrSections.Iy * κₖ
+    elseif κₖ <= kappa_ast
+        ManaR[i] = σY * b * h^2 / 12 * (3 - κₑ^2 / κₖ^2 + κₖ / κₑ * C / E * (2 - 3 * κₑ / κₖ + κₑ^3 / κₖ^3))
+    else
+        zy = epsY / κₖ
+        z0 = eps_ast / κₖ
+        zs = z0 - zy
+        ManaR[i] = 2 * zy^2 * σY * b / 3 + zs * σY * b * (zy + (z0 - zy) / 3)
     end
 end
 
@@ -211,8 +227,14 @@ SEfig = ConstitutiveModelPlot(StrMaterialModels, [-epsY * 3, epsY * 3], 50, 1000
 
 # M-κ plot
 # --------------------------------
-fig = plot(abs.(kappaHistElem[elem, :]), Mana, markershape=:circle, lw=lw, ms=ms, title="M-κ", label="Analytic", minorgrid=1, draw_arrow=1, legend=:bottomright)
-plot!(fig, abs.(kappaHistElem[elem, :]), abs.(mVec), markershape=:rect, lw=lw, ms=ms, label="FEM")
+fig = plot(abs.(kappaHistElem[elem, :]), Mana ./ My, markershape=:circle, lw=lw, ms=ms, title="M-κ", label="Analytic", minorgrid=1, draw_arrow=1, legend=:bottomright)
+plot!(fig, abs.(kappaHistElem[elem, :]), abs.(mVec) ./ My, markershape=:rect, lw=lw, ms=ms, label="FEM")
+xlabel!("κ")
+ylabel!("M")
+
+elem = 1
+fig2 = plot(abs.(kappaHistElem[elem, :]), ManaR ./ My, markershape=:circle, lw=lw, ms=ms, title="M-κ", label="Analytic", minorgrid=1, draw_arrow=1, legend=:bottomright)
+plot!(fig2, abs.(kappaHistElem[elem, :]), abs.(mR) ./ My, markershape=:rect, lw=lw, ms=ms, label="FEM")
 xlabel!("κ")
 ylabel!("M")
 
@@ -223,7 +245,7 @@ maxErrMk = maximum(err)
 
 # P-δ plot  
 # --------------------------------
-fig2 = plot(abs.(dVec), pVec, markershape=:circle, lw=lw, ms=ms, title="P-δ", label="FEM", minorgrid=1, draw_arrow=1, legend=:bottomright)
+fig3 = plot(abs.(dVec), pVec, markershape=:circle, lw=lw, ms=ms, title="P-δ", label="FEM", minorgrid=1, draw_arrow=1, legend=:bottomright)
 xlabel!("δ")
 ylabel!("P")
 
@@ -235,3 +257,24 @@ ndivs = 2
 timesPlot = [1, nLoadSteps]
 
 figsM = BendingMomentPlot(timesPlot, StrMesh, StrPlots, matFint)
+
+
+# Stress plot  
+# --------------------------------
+p, w = gausslegendre(ns)
+
+sfig = plot(σArr[1][convert(Int, ceil(nLoadSteps / 5))], p * h / 2, markershape=:circle, lw=lw, ms=ms, title="stress", label=@sprintf("M = %0.2f", mR[convert(Int, ceil(nLoadSteps / 5))]), minorgrid=1, draw_arrow=1, legend=:bottomright)
+plot!(sfig, σArr[1][convert(Int, ceil(2 * nLoadSteps / 5))], p * h / 2, markershape=:circle, lw=lw, ms=ms, title="stress", label=@sprintf("M = %0.2f", mR[convert(Int, ceil(2 * nLoadSteps / 5))]), minorgrid=1, draw_arrow=1)
+plot!(sfig, σArr[1][convert(Int, ceil(3 * nLoadSteps / 5))], p * h / 2, markershape=:circle, lw=lw, ms=ms, title="stress", label=@sprintf("M = %0.2f", mR[convert(Int, ceil(3 * nLoadSteps / 5))]), minorgrid=1, draw_arrow=1)
+plot!(sfig, σArr[1][convert(Int, ceil(4 * nLoadSteps / 5))], p * h / 2, markershape=:circle, lw=lw, ms=ms, title="stress", label=@sprintf("M = %0.2f", mR[convert(Int, ceil(4 * nLoadSteps / 5))]), minorgrid=1, draw_arrow=1)
+plot!(sfig, σArr[1][end], p * h / 2, markershape=:circle, lw=lw, ms=ms, title="stress", label=@sprintf("M = %0.2f", mR[end]), minorgrid=1, draw_arrow=1)
+plot!(sfig, zeros(length(p)), p * h / 2, lw=lw, ms=ms, label="", color=:"black")
+
+# M- vs M+
+# --------------------------------
+figComp = plot(abs.(mVec), abs.(mR), markershape=:circle, lw=lw, ms=ms, title="M- vs M+", label="FEM", minorgrid=1, draw_arrow=1, legend=:bottomright)
+
+# P vs M-/M+
+# --------------------------------
+figComp2 = plot(abs.(mVec[2:end] ./ mR[2:end]), abs.(pVec[2:end]), markershape=:circle, lw=lw, ms=ms, title="P vs M-/M+", label="FEM", minorgrid=1, draw_arrow=1, legend=:bottomright)
+
